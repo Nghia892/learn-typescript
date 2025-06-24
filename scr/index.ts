e5f9882c9349e994f4862b2715ff5d811fb21308
Nghia892 <trongnghiad25@gmail.com>
2025-06-24 16:23:15 +0700

    Update

--- scr/index.ts
+++ scr/index.ts
@@ -1,89 +1,102 @@
-interface KhachHang {
-    id?: number,
-    tenkh: string,
-    thongtin: string,
-}
-class Khachhang {
-    Khach: KhachHang[] = [];
+// Boolean là 1 loại type ví dụ: True False 
+// let là gán cho 1 biến const là gán cho 1 biến không thể thay đổi
+let Enable: boolean = true
+Enable = false
+const Ena: boolean = true
+// Ena = false -> Sẽ Báo Lỗi
 
-    ThemKhachHang(kh: KhachHang): void {
-        if (kh && kh.tenkh && kh.thongtin) {
-            const NewId = this.Khach.length > 0 ? Math.max(...this.Khach.map(k => k.id ?? 0)) + 1 : 1 ;
-            const RegKhach: KhachHang = {
-                id: NewId,
-                tenkh: kh.tenkh,
-                thongtin: kh.thongtin
-            };
-            this.Khach.push(RegKhach);
-        }
-    }
-    getDanhSachKhach(): KhachHang[] {
-        return this.Khach;
-    }
-}
-interface DonHang {
-    tendon: string,
-    sotien: number
-}
-function CheckKhac(don: DonHang){
-    console.log(don)
-}
-const khachHangManager = new Khachhang();
+// Number : Type chỉ định số 
+let sothutu: Number = 1
+sothutu = 1.5
+// sothutu = "Nghia" -> Không thể gán string vì đây là number
 
-const KhachDauTien: KhachHang = {
-    tenkh: "Trần Bảo Châu",
-    thongtin: "Đến Từ Phú Yên"
-}
+// String -> Type Dạng chữ phải có ""
+let Ten: String = "Nghia"
+Ten = "Rename Nghia"
 
-const KhachDauT2 = {
-    tenkh: "Đỗ Trọng Nghĩa",
-    thongtin: "Đến Từ TPHCM"
-} as KhachHang
+//Void : 1 type cho 1 hàm không trả về giá trị
+// function -> return không trả về 1 giá trị
 
-khachHangManager.ThemKhachHang(KhachDauTien);
-khachHangManager.ThemKhachHang(KhachDauT2);
+//undefined -> hiểu nôn na là 1 giá trị giống null không có gì hoặc chưa khởi tạo
 
-let dathang = {
-    tendon: "Beta Box",
-    sotien: 100
+// Gán vào Khach cho có 2 biến là id là Number, name là String
+// readonly như const khiến id không thể thay đổi giá trị sau này
+type Khach = {
+    readonly id: Number,
+    name: String,
 }
-// generic
-function Generic<Type>(loai: Type): Type{
-    return loai
+// Sử Dụng const để gọi Khachhang chọn type Khach để có 2 biến thông tin là id và name
+const KhacHang: Khach = {id: 1, name: "Name"}
+
+//interface là 1 dạng như type nhưng theo tài liệu có vẻ nó có thể extends , thêm đối tượng vào không lỗi như type
+interface Khach2 {
+    id: Number,
+    name: String,
 }
-interface MoreGeneric {
-    id: number,
-    name: string
+// Bổ sung thêm 1 đối tượng cho Khach2
+interface Khach2 {
+    Active: boolean
 }
-function More<T,U extends MoreGeneric>(loai: T, valuetwo: U): object {
-    console.log(loai, valuetwo)
-    return {
-        loai,
-        valuetwo
+// Lấy Đối Tượng của Khach2 vào KhachThem2
+interface KhachThem2 extends Khach2 {
+    Thongtin: String
+}
+const KhachHang2: KhachThem2 = {id: 1, name: "Nghia", Active: true, Thongtin: "Online"}
+
+//Class là 1 lớp 
+class Player {
+    // Khởi tạo trước 2 biến là username và password sẵn bên trong để có thể constructor mà không lỗi
+    private username: string;
+    public password: number;
+    // Sử dụng constructor để khởi tạo... (chưa hiểu, chưa thể giải thích rõ phần constructor)
+    constructor(username: string, password: number){
+        this.username = username;
+        this.password = password;
     }
 }
-const TestFunc: MoreGeneric = {
-    id: 1,
-    name: "nghia"
+// sử dụng new để tạo 1 profile username là Nghia892 và password là 123123
+const getplayer = new Player("Nghia892", 123123);
+// console.log(getplayer.username) Private -> Chỉ có thể tham chiếu bên trong class
+// console.log(getplayer.password) Public -> có thể tham chiếu phía bên ngoài
+
+// Enum nó khá giống 1 list (Xếp Đầu list là số 0 và tiếp theo là 1)
+enum XepHang {
+    CaoThu,
+    KimCuong,
+    BachKim,
+    Vang
 }
-const CallFunc = More("Nghia", TestFunc)
-// union
-interface A {
-  a: string
+// Ta Cũng có thể gán cho mỗi 1 loại thành 1 string bất kỳ
+enum Ranking {
+    ChienTuong = "Cao Thủ",
+    CaoThu = 5,
+    KimCuong, // KimCuong sẽ số 6
+    BachKim
 }
+console.log(XepHang.CaoThu)
 
-interface B {
-  b: number
+// Array là dạng giá trị nhưng dành cho list [1,2,3]
+//Ví dụ 
+const TheCao: Number[] = [10000,20000,30000]
+//Thường xài Number[] sẽ dễ hơn thay vì xày Array<number>
+//và có cả Vừa Number[] vừa String[] như
+const LoaiThe: (string | number)[] = ["VIETTEL", 500000, "Mobie"]
+
+
+//Tuple 1 kiểu dạng như Array nhưng sẽ là giá trị và biết rõ thứ tự, có thể ứng dụng như
+type Tuple = [String, Number]
+const UngDungTuple: Tuple = ["nghia", 1000] // Sử Dụng Tuple làm type và tạo Array theo String và number theo thứ tự, theo docs thì vẫn có thể áp dụng cho function như
+// tạo 1 function lấy 1 arg là tup và type là Tuple
+function TupleFunc(tup: Tuple){
+    const a = tup[0]; // Gán Cho a là tup[0] theo Array là String
+    const b = tup[1]; // Number
 }
+TupleFunc(["Nghia", 1]) // gọi function = Array
 
-type mathang = string | number
-type AB = A & B
-let gamepass: mathang = "Katana"
-let Mathang2: AB = {a: "1", b: 2}
-gamepass = 1;
-
-
-let LoaiThe = [10000,20000,30000];
-LoaiThe.forEach((item) => console.log(item))
-CheckKhac(dathang);
-console.log(khachHangManager.Khach)
+// Object đối tượng
+// Tạo 1 function lấy User để chọn 2 object là name và id lần lượt String và Number để print ra
+function DocTen(User: {name: String, id: Number}){
+    console.log(User.name, User.id)
+}
+// Chỉ Việc Gọi theo function...
+DocTen({name: "Jonny", id: 1})
